2c3a2444e6d117dbc952e7e923478d24c3da05b8
Ashiq Nazar <khanashiq18@gmail.com>
2022-02-14 17:22:24 +0530

    controller

---
+++ BooksHub/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksHub.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: BookController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
